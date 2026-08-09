112d02e2af60725cc765b194e834027f49ef7b71
Madou DIAKAYETEBA <madoudiakayeteba@gmail.com>
2026-08-09 22:55:41 +0200

    un coup de gap pour le bouton declaration

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,9 @@ import { Link } from "react-router"
 function Home() {
   return (
     <section className='container flex flex-col justify-center items-center min-h-screen'>
-        <article className='bg-white px-10 py-10 flex flex-col justify-center items-center  rounded-md'>
+        <article className='bg-white px-10 py-10 flex flex-col justify-center items-center gap-2  rounded-md'>
             <h1 className='text-3xl'>Gestion des naissances</h1>
-            <Link to= {"/private/declarations"} className='border border-blue-600 text-blue-600 px-6 py-4 rounded-md hover:bg-blue-600 hover:text-white transition-colors'>Déclaration</Link>
+            <Link to= {"/private/declarations"} className='border border-blue-600 text-blue-600 px-6 py-4 rounded-md hover:bg-blue-600 hover:text-white gap-2 transition-colors'>Déclaration</Link>
         </article>
     </section>
   )
